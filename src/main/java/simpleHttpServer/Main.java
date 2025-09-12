e2dcb17cbb8d025d70df132f6aa2cf44b85b0f36
Phong <thieu.phong-le95@gmail.com>
2025-09-12 13:06:12 +0200

    Making a Http Server with Tomcat

--- src/main/java/simpleHttpServer/Main.java
+++ src/main/java/simpleHttpServer/Main.java
@@ -8,7 +8,7 @@ public class Main {
             HttpServer server = new HttpServer(8080);
             server.start();
         } catch (IOException e) {
-            System.out.println("There's a problem ");;
+            System.out.println("There's a problem ");
         }
 
     }
